16c8c98d53638539b8e7159655ea98f813325ba8
shanmughamsb <86920066+shanmughamsb@users.noreply.github.com>
2021-07-05 01:47:17 +0530

    Update Startup.cs

--- Paymentsense.Coding.Challenge.Api/Startup.cs
+++ Paymentsense.Coding.Challenge.Api/Startup.cs
@@ -32,7 +32,7 @@ namespace Paymentsense.Coding.Challenge.Api
                 });
             });
 
-            services.AddResponseCaching();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
